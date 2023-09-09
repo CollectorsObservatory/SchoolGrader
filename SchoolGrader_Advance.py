1d4857fb3c89016dae29cf906464409a3e039fa6
Fares Majdoub <126903635+CollectorsObservatory@users.noreply.github.com>
2023-09-08 22:55:06 -0400

    Update SchoolGrader_Advance.py
    I added GUI with tkinter

--- SchoolGrader_Advance.py
+++ SchoolGrader_Advance.py
@@ -1,24 +1,17 @@
 import random
-import datetime
 import re
-name_pattern = r"^[A-Za-z]+$"  # to enter a correct student name , followed by this function
+import tkinter as tk
+from tkinter import messagebox
+
+name_pattern = r"^[A-Za-z]+$"
 
 
 def is_valid_name(name):
     return bool(re.match(name_pattern, name))
 
 
-now = datetime.datetime.now()  # to display date and time
-time = now.strftime("%Y-%M-%d %H:%M%S")
-
-print("          Welcome to Student Grader Advanced      ")
-print(f"          You logged in at {time}                 ")
-# this is a student grader platform that
-
-
 class Student:
-    """A class that defines the basic information for a student grader"""
-    def __init__(self, name, familyname, grade, code):
+    def __init__(self, name, familyname, grade):
         self.name = name
         self.familyname = familyname
         self.grade = grade
@@ -26,71 +19,113 @@ class Student:
         self.fullname = name + " " + familyname
 
     def studentinfo(self):
-        """Function will be used later in the options of the student grader, same for functions below"""
         return f"{self.fullname} ID: {self.code} has the following grade {self.grade}%"
 
     def studentcode(self):
         return f"{self.fullname} has the following unique student identifier {self.code}"
 
 
-student_list = []  # to store the name for students to access them when needed
-student_codes = {}  # a dic that takes the name of the student and appoints the followingcode
+student_list = []
+student_codes = {}
 student_grades = {}
 
-print("           Press change for more options      ")   # used to simpfly user input and to add students easier
+def add_student():
+    name = name_entry.get()
+    family_name = family_name_entry.get()
+    grade = grade_entry.get()
 
-while True:
-    name = str(input("Please Enter student name : "))
-    if name != "change" and is_valid_name(name): # to keep adding students until the user decides to change the option
-        family_name = str(input(f"Please enter {name}'s family name: "))
-        if is_valid_name(family_name):
-            grade = float(input(f"Please Enter {name}'s grade: "))
-            new_student = Student(name=name, familyname=family_name, grade=grade,
-                                  code=random.randint(100000000, 999999999))
-            print(new_student.studentinfo())
-            student_list.append(new_student)
-            student_codes.update({new_student.code: new_student.fullname})
-            student_grades.update({new_student.fullname: new_student.grade})
-    elif name == "change":  # Displays all the options
-        print("Please choose an action:")
-        print(" ")
-        print("1- Display students' grade")
-        print(" ")
-        print("2- Display students' secret code")
-        print(" ")
-        print("3- Display Student info(Enter unique ID)")
-        print(" ")
-        print("4- Exit")
-        print(" ")
+    if is_valid_name(name) and is_valid_name(family_name):
+        try:
+            grade = float(grade)
+        except ValueError:
+            messagebox.showerror("Invalid Grade", "Please enter a valid grade (a number).")
+            return
 
-        choice = int(input("Choose the option: "))
-
-        if choice == 1:
-            for student in student_list:
-                print(student.studentinfo())
-
-        elif choice == 2:
-            for student in student_list:
-                print(student.studentcode())
-
-        elif choice == 3:
-            student_code = int(input("Enter student secret code: "))
-            student_name = student_codes.get(student_code)
-            student_grade = student_grades[student_name]
-            print(f"the student name is {student_name} and it has the following grade {student_grade}%")
-
-        elif choice == 4:
-            print("Thanks have a good day! ")
-            break
+        new_student = Student(name=name, familyname=family_name, grade=grade)
+        student_list.append(new_student)
+        student_codes.update({new_student.code: new_student.fullname})
+        student_grades.update({new_student.fullname: new_student.grade})
+        update_display(new_student.studentinfo())
+        clear_entries()
+    else:
+        messagebox.showerror("Invalid Name", "Please enter valid names (letters only).")
 
 
+def display_students_info():
+    display_text.delete(1.0, tk.END)
+    for student in student_list:
+        display_text.insert(tk.END, student.studentinfo() + "\n")
 
 
+def display_students_code():
+    display_text.delete(1.0, tk.END)
+    for student in student_list:
+        display_text.insert(tk.END, student.studentcode() + "\n")
 
 
+def display_student_info():
+    student_code = int(student_code_entry.get())
+    student_name = student_codes.get(student_code)
+    if student_name:
+        student_grade = student_grades[student_name]
+        display_text.delete(1.0, tk.END)
+        display_text.insert(tk.END, f"The student name is {student_name} and has the following grade {student_grade}%\n")
+    else:
+        messagebox.showerror("Student Not Found", "Student with the provided code not found.")
 
 
+def update_display(message):
+    display_text.insert(tk.END, message + "\n")
 
 
+def clear_entries():
+    name_entry.delete(0, tk.END)
+    family_name_entry.delete(0, tk.END)
+    grade_entry.delete(0, tk.END)
+    student_code_entry.delete(0, tk.END)
 
 
+# Create the main window
+root = tk.Tk()
+root.title("Student Grader")
+
+# Create and place GUI components
+name_label = tk.Label(root, text="Name:")
+name_label.pack()
+name_entry = tk.Entry(root)
+name_entry.pack()
+
+family_name_label = tk.Label(root, text="Family Name:")
+family_name_label.pack()
+family_name_entry = tk.Entry(root)
+family_name_entry.pack()
+
+grade_label = tk.Label(root, text="Grade:")
+grade_label.pack()
+grade_entry = tk.Entry(root)
+grade_entry.pack()
+
+add_button = tk.Button(root, text="Add Student", command=add_student)
+add_button.pack()
+
+display_students_button = tk.Button(root, text="Display Students' Grade", command=display_students_info)
+display_students_button.pack()
+
+display_code_button = tk.Button(root, text="Display Students' Code", command=display_students_code)
+display_code_button.pack()
+
+student_code_label = tk.Label(root, text="Enter Student Code:")
+student_code_label.pack()
+student_code_entry = tk.Entry(root)
+student_code_entry.pack()
+
+display_student_button = tk.Button(root, text="Display Student Info", command=display_student_info)
+display_student_button.pack()
+
+display_text = tk.Text(root, height=10, width=50)
+display_text.pack()
+
+quit_button = tk.Button(root, text="Quit", command=root.destroy)
+quit_button.pack()
+
+root.mainloop()
